10b9d72e590a385d841685d8a61409d1b5a8d746
avneesh_lenevo <singhtest20@gmail.com>
2022-08-14 21:30:26 +0100

    init commit

---
+++ src/main/java/com/singh/tenancy/TenancyRaServerApplication.java
@@ -0,0 +1,13 @@
+package com.singh.tenancy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TenancyRaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TenancyRaServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/singh/tenancy/TenancyRaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.singh.tenancy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TenancyRaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
